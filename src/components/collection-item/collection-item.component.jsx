fbd72ff8017cd3314db828e6911ac78394775092
Aaron Flores <aaron.flores@assetworks.com>
2020-06-03 09:39:57 -0700

    updates

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import './collection-item.styles.scss';
+import CustomButton from '../custom-button/custom-button.component';
+import { connect } from 'react-redux';
+import { AddItem } from '../../redux/cart/cart.actions';
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
+const CollectionItem = ({item, add}) => {
+    const { name, price, imageUrl } = item;
+
+    return (
     <div className='collection-item'>
         <div
             className='image'
@@ -12,8 +18,15 @@ const CollectionItem = ({id, name, price, imageUrl}) => (
         <div className='collection-footer'>
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
-        </div>        
+        </div>   
+        <CustomButton onClick={() => add(item)} inverted>
+            Add to cart
+        </CustomButton>     
     </div>
-);
+)};
 
-export default CollectionItem;
+const mapDispatchToPros = dispatch => ({
+    add: item => dispatch(AddItem(item))
+});
+
+export default connect(null, mapDispatchToPros)(CollectionItem);
