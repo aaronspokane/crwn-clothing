db677ffd79c7df61deb45df3495eb008ec9d7472
Aaron Flores <aaron.flores@assetworks.com>
2020-06-04 21:40:29 -0700

    Updates

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,6 +4,7 @@ import './cart-icon.styles.scss';
 import { connect } from 'react-redux';
 import { ToggleCartHidden } from '../../redux/cart/cart.actions';
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
+import { createStructuredSelector } from 'reselect';
 
 const CartIcon = ({ toggleCart, itemCount }) => (
    
@@ -17,8 +18,8 @@ const mapDispatchToProps = dispatch => ({
     toggleCart: () => dispatch(ToggleCartHidden())
 })
 
-const mapStateToProps = state => ({
-    itemCount: selectCartItemsCount(state)
+const mapStateToProps = createStructuredSelector({
+    itemCount: selectCartItemsCount
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
